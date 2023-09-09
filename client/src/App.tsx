2798fbf7b672285c0a5f3d52d9f434cff906b790
Max <allenmaxnichols@gmail.com>
2023-09-08 23:29:51 -0500

    message interval and send to server

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react';
+import {Container, Button, Input } from '@mantine/core';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [messages, setMessages] = useState<string[]>([])
+  const [userMessage, setUserMessage] = useState<string>('');
+  const [socket, setSocket] = useState<WebSocket | null>(null);
+
+
+  const handleSend = () => {
+    console.log(socket);
+    if (!socket) return;
+    console.log(userMessage)
+    socket.send(userMessage);
+    setUserMessage('');
+  }
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserMessage(e.target.value);
+  }
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8000');
+    setSocket(ws);
+
+    ws.onopen = () => {
+      console.log('connected');
+    }
+
+    ws.onmessage = (e) => {
+      console.log(e.data);
+      // setMessages((prev) => [...prev, e.data])
+    }
+
+    ws.onclose = () => {
+      console.log('disconnected');
+    }
+
+    return () => {
+      ws.close()
+    }
+
+  }, [])
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container>
+       <Input placeholder="Your message" onChange={handleInput} />
+       <Button onClick={handleSend}>Send</Button>
+    </Container>
   )
 }
 
--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,8 @@
 import { WebSocketServer } from 'ws';
 import http from 'http';
-import UUID from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 const server = http.createServer();
-
 
 const ws = new WebSocketServer({ server });
 const PORT = 8000;
@@ -12,15 +11,29 @@ server.listen(PORT, () => {
     console.log(`ws is running on ${PORT}`);
 })
 
-const clients = {};
+
+function randomMessage(){
+    const messages = ['hello', 'hi', 'bye', 'goodbye', 'hola', 'adios', 'bonjour', 'au revoir', 'ciao', 'arrivederci', 'hallo', 'tschuss'];
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    return messages[randomIndex];
+}
 
 ws.on('connection', function connection(ws){
-    // Generate a unique code for every user
-    const userId = UUID.v4();
-    console.log(`recieved a connection`);
 
-    // Store unique code in clients object
-    clients[userId] = ws;
-    console.log(`${userId} connected`);
+    const message = setInterval(() => {
+        ws.send(randomMessage())
+        console.log('sent message')
+    }, 10000);
+
+    ws.on('message', function incoming(message){
+        console.log('received: %s', message);
+    });
+
+    ws.on('close', function close(){
+        console.log('disconnected');
+        clearInterval(message);
+    })
+
 
 })
+
