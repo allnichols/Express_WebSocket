e34ca9aec2e91da6863b3181de6b69a960a07ac9
Max <allenmaxnichols@gmail.com>
2023-09-13 17:35:25 -0500

    add db schema

--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,6 +1,17 @@
 import app from "../config/express";
-
+import pool from "../config/db";
 const port = process.env.PORT || 8000;
+
+
+app.get("/", (req, res) => {
+    pool.query("SELECT * FROM users", (err, result) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200).json(result.rows);
+    });
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
---
+++ server/src/database.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE chat_app;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE chat_rooms (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
+    visibility BOOLEAN DEFAULT TRUE,
+    creator_id INTEGER REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE chat_room_members (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    room_id INTEGER REFERENCES chat_rooms(id),
+    joined_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    room_id INTEGER REFERENCES chat_rooms(id),
+    message TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
+);
