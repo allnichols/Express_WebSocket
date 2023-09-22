c096b6dcf207a2cd6487a1723e8e854052552998
Max <allenmaxnichols@gmail.com>
2023-09-22 16:42:50 -0500

    add header styling

--- client/src/components/header/index.tsx
+++ client/src/components/header/index.tsx
@@ -2,10 +2,21 @@ import { Container, Tabs } from "@mantine/core";
 import classes from "./header.module.css";
 
 export default function SideBar() {
+  // get all rooms and chats
+  // show sidebar with rooms and chats
+  // when user clicks on room or chat, show room or recent chat
   return (
     <div className={classes.header}>
       <Container className={classes.container}>
-        <Tabs className={classes.tabs} variant="outline" color="blue">
+        <Tabs
+          classNames={{
+            root: classes.tabs,
+            list: classes.tabsList,
+            tab: classes.tab,
+          }}
+          variant="outline"
+          color="blue"
+        >
           <Tabs.List>
             <Tabs.Tab className={classes.tab} value="Chat">
               Chat
